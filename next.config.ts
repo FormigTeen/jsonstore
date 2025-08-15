30543b0f57e2fc61c529327e1d0d2db64212606c
Matheus Freitas <msformigteen@live.com>
2025-08-15 12:39:55 -0300

    feat: change output

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+    output: "standalone",
     images: {
         unoptimized: true, // necessário para export estático
         remotePatterns: [
